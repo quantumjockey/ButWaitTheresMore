582739e97ee02d3bd80490b78aec70cf8ffef06f
quantumjockey <nickyd242@gmail.com>
2014-05-18 00:33:27 -0700

    Step one.

---
+++ ButWaitTheresMore/ObjectExtensions.cs
@@ -0,0 +1,35 @@
+﻿///////////////////////////////////////
+#region Namespace Directives
+
+using System.Reflection;
+
+#endregion
+///////////////////////////////////////
+
+namespace ButWaitTheresMore
+
+{
+    public static class ObjectExtensions
+    {
+        ////////////////////////////////////////
+        #region Supporting Methods
+
+        public static bool HasNullValues(this object _dummyObject)
+        {
+            bool _hasNullValues = false;
+            PropertyInfo[] properties = _dummyObject.GetType().GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                if (property.GetValue(_dummyObject, null) == null)
+                {
+                    _hasNullValues = true;
+                    break;
+                }
+            }
+            return _hasNullValues;
+        }
+
+        #endregion
+    }
+}
