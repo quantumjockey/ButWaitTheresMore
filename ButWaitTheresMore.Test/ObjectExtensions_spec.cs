bbb0a42294972006ebc7afb5178ab525f9b23744
quantumjockey <nickyd242@gmail.com>
2014-05-18 01:03:35 -0700

    Added test and modeling components.

---
+++ ButWaitTheresMore.Test/ObjectExtensions_spec.cs
@@ -0,0 +1,67 @@
+﻿///////////////////////////////////////
+#region Namespace Directives
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#endregion
+///////////////////////////////////////
+
+namespace ButWaitTheresMore.Test
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class ObjectExtensions_spec
+    {
+        ////////////////////////////////////////
+        #region Constructor (Auto-generated)
+
+
+        public ObjectExtensions_spec()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        #endregion
+
+        ////////////////////////////////////////
+        #region TestContext Components (Auto-Generated)
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #endregion
+
+        ////////////////////////////////////////
+        #region Unit Tests (Methods)
+
+
+
+        #endregion
+
+        ////////////////////////////////////////
+        #region Child Classes (Used in Testing)
+
+
+
+        #endregion
+    }
+}
--- ButWaitTheresMore/ObjectExtensions.cs
+++ ButWaitTheresMore/ObjectExtensions.cs
@@ -1,19 +1,24 @@
 ﻿///////////////////////////////////////
 #region Namespace Directives
 
+using System.Collections.Generic;
 using System.Reflection;
 
 #endregion
 ///////////////////////////////////////
 
 namespace ButWaitTheresMore
-
 {
     public static class ObjectExtensions
     {
         ////////////////////////////////////////
-        #region Supporting Methods
+        #region Null Values
 
+        /// <summary>
+        /// Tests a custom object for null property values.
+        /// </summary>
+        /// <param name="_dummyObject">The object to be tested.</param>
+        /// <returns>Whether or not the object contains properties with null values.</returns>
         public static bool HasNullValues(this object _dummyObject)
         {
             bool _hasNullValues = false;
@@ -28,6 +33,26 @@ namespace ButWaitTheresMore
                 }
             }
             return _hasNullValues;
+        }
+
+        /// <summary>
+        /// Searches a custom object for null property values.
+        /// </summary>
+        /// <param name="_dummyObject">The object to be searched.</param>
+        /// <returns>A list of properties with null values.</returns>
+        public static List<string> ListNullValues(this object _dummyObject)
+        {
+            List<string> _nullValues = new List<string>();
+            PropertyInfo[] properties = _dummyObject.GetType().GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                if (property.GetValue(_dummyObject, null) == null)
+                {
+                    _nullValues.Add(property.Name);
+                }
+            }
+            return _nullValues;
         }
 
         #endregion
